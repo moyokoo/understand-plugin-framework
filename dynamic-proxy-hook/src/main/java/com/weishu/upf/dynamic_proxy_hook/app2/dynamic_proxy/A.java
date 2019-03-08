9b15943d341ba5f7a4d4c06aee7cb5089f0d23a8
moyokoo <1604165677@qq.com>
2019-03-08 17:08:17 +0800

    1

---
+++ dynamic-proxy-hook/src/main/java/com/weishu/upf/dynamic_proxy_hook/app2/dynamic_proxy/A.java
@@ -0,0 +1,9 @@
+package com.weishu.upf.dynamic_proxy_hook.app2.dynamic_proxy;
+
+import android.util.Log;
+
+public class A {
+    void print(String name) {
+        Log.e("1", "A print name is " + name);
+    }
+}
